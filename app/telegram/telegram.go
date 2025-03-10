822d5bfcaf4bae2debdec4164c6ac0784ceeee2d
Psticso <Psticso@outlook.com>
2025-03-10 19:47:49 +0800

    Update telegram.go

--- app/telegram/telegram.go
+++ app/telegram/telegram.go
@@ -3,7 +3,7 @@ package telegram
 import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/v03413/bepusdt/app/config"
+	"github.com/Psticso/bepusdt/app/config"
 	"strconv"
 )
 
